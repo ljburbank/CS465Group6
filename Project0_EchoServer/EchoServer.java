b06d699916813483a61a083f89655f24052b5385
Logan Burbank <ljb298@nau.edu>
2021-01-22 11:44:33 -0700

    Updated EchoServer code

--- Project0_EchoServer/EchoServer.java
+++ Project0_EchoServer/EchoServer.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -31,52 +28,121 @@ class EchoServer
 
     private static class EchoThread implements Runnable
     {
-        Socket clientSocket;
-
-        EchoThread(Socket client)
+	//class variables
+        private Socket client;
+    
+    	/**
+     	* EchoThread initializes thread object with given socket connection.
+     	* @param client - socket connection to be handled by thread.
+     	*/
+    	
+	public EchoThread( Socket client )
         {
-            clientSocket = client;
+            this.client = client;
         }
-
-        @Override
-        public void run()
+    
+    
+    
+    /**
+     * Run function opens up data IO steams with client then listens.
+     * Reads characters from client one at a time. Processes each character as follows:
+     *  - ignores characters that are not letters.
+     *  - looks for the sequence "quit", terminates connection if found.
+     *  - all characters that are letters are echoed back to the client.
+     */
+    @Override
+    public void run() 
         {
-            try
-            (
-                PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            )   {
-                    String inputLine, outputLine;
+         //initialize function / variables
+         DataInputStream fromClient;  //data stream from client
+         DataOutputStream toClient;   //data stream to client
+         char charFromClient;         //character received from client via input stream
+         char workingChar;            //copy of clientData for lossless processing
+         char prevChar = ' ';         //default init value, serves no purpose
+         Boolean listen = true;       //control boolean for listening while loop
+         
+         try  //IOException possible, must be caught.
+             {
+              //get IO streams
+              fromClient = new DataInputStream( client.getInputStream() );
+              toClient = new DataOutputStream( client.getOutputStream() );
+             
+              //while loop to listen to client
+              while( listen )
+                  {
+                   //read input from client
+                   charFromClient = (char) fromClient.readByte();
+                   System.out.println(charFromClient);
+                  
+                   //process char from user: check if letter, check for "quit"
+                   //check if char is letter
+                   if( Character.isLetter( charFromClient ) )
+                       {
+                        //if letter, make lowercase, working copy
+                        workingChar = Character.toLowerCase( charFromClient );
+                        
+                        //if letter is q
+                        if( workingChar == 'q' )
+                            {
+                             //note q seen
+                             prevChar = 'q';
+                             
+                            }
+                        
+                        //else if q is seen, check for u
+                        else if ( prevChar == 'q' && workingChar == 'u')
+                            {
+                             //note u seen
+                            prevChar = 'u';
+                            }
+                        
+                        //else if u seen, check for i
+                        else if( prevChar == 'u' && workingChar == 'i' )
+                            {
+                             //note i seen
+                             prevChar = 'i';
+                            }
+                        
+                        //else if i seen, check for t
+                        else if( prevChar == 'i' && workingChar == 't' )
+                            {
+                             //note t seen
+                             prevChar = 't';
+                            
+                            }
+                        
+                        //else quit sequence broken, clear prevChar.
+                        else
+                            {
+                             prevChar = ' ';
+                            }
+                        
+                        //echo character to client
+                        toClient.writeByte( clientData );
 
-                    while((inputLine = fromClient.readLine()) != null)
-                    {
-                        outputLine = processInput(inputLine);
-                        toClient.println(outputLine);
-                        if(outputLine.toLowerCase().contains("quit"))
-                        {
-                            System.out.println("Connection Lost");
-                            break;
-                        }
-                    }
-                } catch(IOException err)
-                {
-                    err.printStackTrace();
-                }
-        }
-
-        private String processInput(String inputLine)
-        {
-            String resultLine = "";
-            for(int i = 0; i < inputLine.length(); i++)
-            {
-                if( (inputLine.charAt(i) >= 'a' && inputLine.charAt(i) <= 'z') ||
-                    (inputLine.charAt(i) >= 'A' && inputLine.charAt(i) <= 'Z') )
-                {
-                    resultLine += inputLine.charAt(i);
-                }
-            }
-            return resultLine;
-        }
+                        //check for 'quit' seen
+                        if( prevChar == 't' )
+                            {
+                             listen = false; //exit server while loop
+                            }
+                       }
+                       //end character processing
+                   
+                  }
+                  //end server while loop
+              client.close();
+             } 
+        
+         //catch block for io exceptions: print exception, continue.
+         catch (IOException e)
+             {
+              //print stack trace
+              e.printStackTrace();
+            
+             }
+        
+        
+         }
 
     }
 }
