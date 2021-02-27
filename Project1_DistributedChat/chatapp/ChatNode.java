1e2296c19e55bc5b82a9a98e54ed06a19492bd75
bwarman16 <33626534+bwarman16@users.noreply.github.com>
2021-02-26 22:00:32 -0700

    Add files via upload

--- Project1_DistributedChat/chatapp/ChatNode.java
+++ Project1_DistributedChat/chatapp/ChatNode.java
@@ -3,7 +3,22 @@ package chatapp;
 import java.util.LinkedList;
 
 public class ChatNode
-{
+  {
+
+    public class NodeInfo
+      {
+        String IP;
+        String Name;
+        int Port;
+
+        public NodeInfo(String IP, String inName, int inPort)
+          {
+            this.IP = IP;
+            Name = inName;
+            Port = inPort;
+          }
+      }
+
     static Receiver receiver = null;
     static Sender sender = null;
 
@@ -11,20 +26,27 @@ public class ChatNode
     public static LinkedList<NodeInfo> participantList;
 
     public ChatNode(String inIp, String inName, int inPort)
-    {
-        myInfo = new NodeInfo( inIp, inName, inPort );
+      {
+        myInfo = new NodeInfo(inIp, inName, inPort);
 
         participantList = new LinkedList<NodeInfo>();
-    }
+      }
 
     public void run()
-    {
+      {
         (receiver = new Receiver(myInfo)).start();
         (sender = new Sender()).start();
-    }
+      }
 
-    public static void main( String[] args )
-    {
-        (new ChatNode( args[0], args[1], Integer.parseInt(args[2]) )).run();
-    }
-}
+    public static void main(String[] args)
+      {
+        (new ChatNode(args[0], args[1], Integer.parseInt(args[2]))).run();
+      }
+  }
+
+/*
+ * public class NodeInfo { String IP; String Name; int Port;
+ * 
+ * public NodeInfo(String IP, String inName, int inPort) { this.IP = IP; Name =
+ * inName; Port = inPort; } }
+ */
--- Project1_DistributedChat/chatapp/Receiver.java
+++ Project1_DistributedChat/chatapp/Receiver.java
@@ -2,36 +2,39 @@ package chatapp;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import chatapp.ChatNode.NodeInfo;
 
 public class Receiver extends Thread
-{
+  {
     static ServerSocket receiverSocket = null;
 
-    public Receiver( NodeInfo myNode )
-    {
+    public Receiver(NodeInfo myNode)
+      {
         try
-        {
-            receiverSocket = new ServerSocket( myNode.getPort() );
-        }
-        catch ( IOException err )
-        {
+          {
+            receiverSocket = new ServerSocket(myNode.Port);
+          } 
+        
+        catch (IOException err)
+          {
             err.printStackTrace();
-        }
-    }
+          }
+      }
 
     @Override
     public void run()
-    {
-        while( true )
-        {
+      {
+        while (true)
+          {
             try
-            {
-                ( new ReceiverWorker( receiverSocket.accept() ) ).start();
-            }
-            catch ( IOException err )
-            {
+              {
+                (new ReceiverWorker(receiverSocket.accept())).start();
+              }
+
+            catch (IOException err)
+              {
                 err.printStackTrace();
-            }
-        }
-    }
-}
+              }
+          }
+      }
+  }
