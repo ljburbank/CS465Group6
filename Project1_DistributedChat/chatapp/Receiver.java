23d55b8e8c34b817b7c31e4b04deb4ee67d2074f
StuckInsideJake <66990588+StuckInsideJake@users.noreply.github.com>
2021-02-28 21:36:04 -0700

    Update Receiver.java

--- Project1_DistributedChat/chatapp/Receiver.java
+++ Project1_DistributedChat/chatapp/Receiver.java
@@ -3,10 +3,14 @@ package chatapp;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+
+// class declaration, Reciever has its own thread.
 public class Receiver extends Thread
 {
     static ServerSocket receiverSocket = null;
 
+    // init constructor
+    // allows recieverSocket to be set to a new serverSocket
     public Receiver( NodeInfo myNode )
     {
         try
@@ -19,6 +23,7 @@ public class Receiver extends Thread
         }
     }
 
+    // thread runs
     @Override
     public void run()
     {
